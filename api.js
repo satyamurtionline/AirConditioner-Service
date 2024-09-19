f703066701a49536c87f4736dcf848b22574f7d4
Baiju nath <154792959+satyamurtionline@users.noreply.github.com>
2024-09-19 19:36:24 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,16 +1,1 @@
-const url = 'https://rapid-porn.p.rapidapi.com/photo?category=ass';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '79ee327c44mshb131b363929a714p176e2ajsn2cb538f9e706',
-		'x-rapidapi-host': 'rapid-porn.p.rapidapi.com'
-	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
